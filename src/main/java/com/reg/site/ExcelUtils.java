313f35fbdf20ddf40bea511fa2f1ac21dcad3d59
SynthCannon <niksum15@gmail.com>
2018-05-23 22:39:58 +0100

    added Excel capabilities

---
+++ src/main/java/com/reg/site/ExcelUtils.java
@@ -0,0 +1,76 @@
+package com.reg.site;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class ExcelUtils {
+    ////////////////////// Attributes //////////////////////
+	
+	private static XSSFSheet ExcelWSheet;
+	private static XSSFWorkbook ExcelWBook;
+	private static XSSFCell Cell;
+	private static XSSFRow Row;
+
+    ////////////////////// Methods //////////////////////
+	
+	public static void setExcelFile(String Path, int sheetIndex) {
+		try {
+			FileInputStream ExcelFile = new FileInputStream(Path);
+			ExcelWBook = new XSSFWorkbook(ExcelFile);
+			ExcelWSheet = ExcelWBook.getSheetAt(sheetIndex);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+	public static XSSFSheet getExcelWSheet() {
+		return ExcelWSheet;
+	}
+
+	public static String getCellData(int RowNum, int ColNum) {
+		try {
+			Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
+			String CellData = Cell.getStringCellValue();
+
+			return CellData;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+
+	public static void setCellData(String Result, int RowNum, int ColNum) {
+		try {
+			Row = ExcelWSheet.getRow(RowNum);
+			Cell = Row.getCell(ColNum, MissingCellPolicy.RETURN_BLANK_AS_NULL);
+
+			if (Cell == null) {
+				Cell = Row.createCell(ColNum);
+				Cell.setCellValue(Result);
+			} else {
+				Cell.setCellValue(Result);
+			}
+
+			FileOutputStream fileOut = new FileOutputStream(Constants.PATH + Constants.TESTDATA);
+			ExcelWBook.write(fileOut);
+
+			fileOut.flush();
+			fileOut.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
